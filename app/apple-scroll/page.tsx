ca5d0fec4c6f5c91b09ceebeb5cfc4bb071acdd3
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-05-11 17:37:34 +0330

    apple scroll

---
+++ app/apple-scroll/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { TextParallaxContentExample } from './components/TextParallaxScroll'
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <div>
+      <TextParallaxContentExample />
+    </div>
+  )
+}
+
+export default page
